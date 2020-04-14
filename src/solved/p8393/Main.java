3c4f5eec3e52a386e31d26800227c0d5f6d0763a
Daeho Park <dhpark0648@gmail.com>
2020-04-14 12:52:55 +0900

    solve 8393

---
+++ src/solved/p8393/Main.java
@@ -0,0 +1,16 @@
+package solved.p8393;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        int sum = (n * (n + 1)) / 2;
+        System.out.println(sum);
+    }
+
+}
