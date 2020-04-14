ca7b81fe15ab3631205d3776364747b9f3cfd104
Daeho Park <dhpark0648@gmail.com>
2020-04-14 11:42:07 +0900

    solve 2588

---
+++ src/solved/p2588/Main.java
@@ -0,0 +1,19 @@
+package solved.p2588;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int temp = b;
+        while (temp != 0) {
+            System.out.println(a * (temp % 10));
+            temp /= 10;
+        }
+        System.out.println(a * b);
+    }
+
+}
