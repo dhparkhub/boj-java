a34a2170232f91bde9081aa96a70d8664caaa7b7
Daeho Park <dhpark0648@gmail.com>
2020-04-14 13:08:57 +0900

    solve 11021

---
+++ src/solved/p11021/Main.java
@@ -0,0 +1,23 @@
+package solved.p11021;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int t = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(String.format("Case #%d: %d", i, a + b));
+            bw.newLine();
+        }
+        bw.flush();
+    }
+
+}
