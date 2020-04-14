ec00e9d5a66d38edf5bbefd723eb0e20176da5ee
Daeho Park <dhpark0648@gmail.com>
2020-04-14 13:00:45 +0900

    solve 15552

---
+++ src/solved/p15552/Main.java
@@ -0,0 +1,23 @@
+package solved.p15552;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int t = Integer.parseInt(st.nextToken());
+        while (t-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(String.valueOf(a + b));
+            bw.newLine();
+        }
+        bw.flush();
+    }
+
+}
