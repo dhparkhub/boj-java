5344ed3410a54d35b7e5cf338b84bfad021f967f
Daeho Park <dhpark0648@gmail.com>
2020-04-14 18:53:00 +0900

    solve 10818

---
+++ src/solved/p10818/Main.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        int min = 1000000;
+        int max = 1;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<n; i++) {
+            int temp = Integer.parseInt(st.nextToken());
+            min = Math.min(min, temp);
+            max = Math.max(max, temp);
+        }
+
+        System.out.println(String.format("%d %d", min, max));
+    }
+
+}
